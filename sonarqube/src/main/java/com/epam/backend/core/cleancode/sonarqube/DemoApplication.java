6ac2ba7f0b40a4c67769cd6ed981493645290a62
Magdalena <Magdalena_Zaba@epam.com>
2024-12-16 11:48:24 +0100

    add duplicates

--- sonarqube/src/main/java/com/epam/backend/core/cleancode/sonarqube/DemoApplication.java
+++ sonarqube/src/main/java/com/epam/backend/core/cleancode/sonarqube/DemoApplication.java
@@ -45,14 +45,20 @@ class SampleController {
 
     @GetMapping("/duplicate-code")
     public String duplicateCodeExample(String input) {
-        // Duplicated code from unsafeMethod
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "password");
-            Statement statement = connection.createStatement();
-            statement.executeQuery("SELECT * FROM user_data WHERE user_name = '" + input + "'");
-            return "Data Retrieved";
-        } catch (SQLException e) {
-            return "Error in SQL Handling";
+        // Duplicated code from complexMethod
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 20; j++) {
+                result += i * j;
+            }
+        }
+
+        if (result > 1000) {
+            return "Large Result";
+        } else if (result > 500) {
+            return "Medium Result";
+        } else {
+            return "Small Result";
         }
     }
 
